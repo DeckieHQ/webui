cc3efa8d812383586708f8cb89b923f711c05fc6
Forster Groux <groux.forster@gmail.com>
2016-01-18 19:36:19 +0100

    extend new validated-input to login

---
+++ app/controllers/login.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+import EmberValidations from 'ember-validations';
+
+const { service } = Ember.inject;
+
+export default Ember.Controller.extend(EmberValidations, {
+  session: service('session'),
+
+  validations: {
+    email: {
+      presence: true
+    },
+    password: {
+      presence: true
+    }
+  },
+
+  actions: {
+    authenticate: function() {
+      let identification = this.get('email');
+      let password = this.get('password');
+
+      let self = this;
+
+      return self.validate()
+        .then(() => this.get('session').authenticate('authenticator:devise', identification, password))
+            // .catch((reason) => {
+            //   this.set('errorMessage', reason.error);
+            // })
+        // )
+        .catch(() => {
+          self.set("showErrors", true);
+        });
+    }
+  }
+});

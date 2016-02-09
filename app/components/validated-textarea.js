c87912b784ae69b216c6d9322ae89c04a86f7d45
Forster Groux <groux.forster@gmail.com>
2016-02-09 17:20:06 +0100

    add characters left info for texarea component

--- app/components/validated-textarea.js
+++ app/components/validated-textarea.js
@@ -1,3 +1,8 @@
+import Ember from 'ember';
 import ValidatedInput from '../components/validated-input';
 
-export default ValidatedInput.extend();
+export default ValidatedInput.extend({
+  characters_left: function() {
+    return this.get('maxlength') - this.get('value').length;
+  }.property('value'),
+});

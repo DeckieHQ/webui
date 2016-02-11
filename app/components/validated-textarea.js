9a59aef0dd5e3702a28c3707394249ca9baaa9af
Forster Groux <groux.forster@gmail.com>
2016-02-11 18:24:37 +0100

    fix a bug on validated-textarea

--- app/components/validated-textarea.js
+++ app/components/validated-textarea.js
@@ -3,6 +3,6 @@ import ValidatedInput from '../components/validated-input';
 
 export default ValidatedInput.extend({
   characters_left: function() {
-    return this.get('maxlength') - this.get('value').length;
+    return this.get('maxlength') - (this.get('value') || "").length;
   }.property('value'),
 });

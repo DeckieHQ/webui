128ac61b97997d8a3d1040b9428023da6b716570
Forster Groux <groux.forster@gmail.com>
2016-02-26 18:29:35 +0100

    add validated-date

---
+++ app/components/validated-date.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import ValidatedInput from '../components/validated-input';
+
+export default ValidatedInput.extend({
+  //TODO: focus events are not merged on ember bootsrap datepicker, check later if it is
+});

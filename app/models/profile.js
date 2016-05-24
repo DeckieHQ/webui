2b5217afb4bb1d996764ecec0d73b29b3a8d5345
Forster Groux <groux.forster@gmail.com>
2016-05-24 17:44:13 +0200

    add ember cropper

--- app/models/profile.js
+++ app/models/profile.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   nickname: DS.attr(),
   display_name: DS.attr(),
   avatar_url: DS.attr(),
+  avatar: DS.attr(),
   short_description: DS.attr(),
   description: DS.attr(),
   email_verified: DS.attr(),

381bc4daac32b00cd930499a110df7e8df77f630
Forster Groux <groux.forster@gmail.com>
2016-06-02 16:15:46 +0200

    improve avatar crop box

--- app/components/image-input.js
+++ app/components/image-input.js
@@ -3,14 +3,28 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   file: null,
   displayCropper: false,
+  loadImage: false,
+  wrongFormat: false,
+  validTypes: [ 'image/jpeg', 'image/jpg', 'image/png', 'image/bmp', 'image/gif'],
 
   actions: {
     fileSelectionChanged: function(file) {
-      this.set('displayCropper', false);
-      let self = this;
-      setTimeout(function(){self.set('displayCropper', true);}, 1000);
-      this.set('file', file);
-      this.set('profile.avatar', file.dataURL);
+      if(this.get('validTypes').indexOf(file.type) >= 0) {
+        this.set('displayCropper', false);
+        this.set('wrongFormat', false);
+        this.set('loadImage', true);
+        let self = this;
+        setTimeout(function(){
+          self.set('loadImage', false);
+          self.set('displayCropper', true);
+        }, 1000);
+        this.set('file', file);
+        this.set('profile.avatar', file.dataURL);
+      } else {
+        this.set('displayCropper', false);
+        this.set('loadImage', false);
+        this.set('wrongFormat', true);
+      }
     },
 
     save: function(defer) {

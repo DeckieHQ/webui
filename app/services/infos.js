45f4bd669805ef979f5b59da1018fff87cbaa3fa
Forster Groux <groux.forster@gmail.com>
2016-06-15 18:45:55 +0200

    improve infos right bar

--- app/services/infos.js
+++ app/services/infos.js
@@ -1,10 +1,43 @@
 import Ember from 'ember';
+import ENV from '../config/environment';
 
 export default Ember.Service.extend({
   currentUser: Ember.inject.service(),
 
   init() {
     this._super(...arguments);
+    if (!navigator.geolocation) { return this._loadSearch(); }
+
+    let self = this;
+
+    return new Promise(function(resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    }).then(function(position) {
+      return self._loadSearch(position.coords);
+    }).catch(function(err) {
+      return self._loadSearch();
+    });
+  },
+
+  _loadSearch: function(coords = null) {
+    let client = instantsearch(ENV.algolia).client,
+         index = client.initIndex(ENV.algolia.indexName),
+         searchParams = {};
+
+     if (coords != null) {
+       searchParams = { aroundLatLng: `${coords.latitude}, ${coords.longitude}` };
+     }
+
+     let self = this;
+
+    return new Promise(function(resolve, reject) {
+      index.search('', searchParams, function searchDone(err, content) {
+        if (err) { return reject(err); }
+
+        self.set('events', content.hits.slice(0, 3));
+        resolve(content.hits.slice(0, 3));
+      });
+    });
   },
 
   hosted_events: function(){

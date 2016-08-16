4046ee62e96148a118bfd012ef68277e453cddff
Adrien Folie <folie.adrien@gmail.com>
2016-08-16 17:18:44 +0200

    add flexible events to hosted events

--- app/components/plus-one.js
+++ app/components/plus-one.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['inline'],
+  attendeesCount: function () {
+    return this.get('event.attendees_count') + 1;
+  }.property('event'),
 
-  plusOne: function () {
-    return this.get('count') + 1;
-  }.property('count')
+  minCapacity: function () {
+    return this.get('event.min_capacity') + 1;
+  }.property('event'),
+
+  capacity: function () {
+    return this.get('event.capacity') + 1;
+  }.property('event'),
 });

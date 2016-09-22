e06a9027c5ab7983653aefa923d621278e9cbfa8
Forster Groux <groux.forster@gmail.com>
2016-09-22 15:39:38 +0200

    add hosted events opened on profile

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('profile', params.profile_id);
+  },
+
+  afterModel(model) {
+    let params = {
+      sort: 'begin_at',
+      filters: {
+        opened: true
+      },
+    };
+
+    return model.query('hosted_events', params);
   }
 });
